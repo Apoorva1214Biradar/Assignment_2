513e7294181b2be33f4cc83057179b5ef1acf2f0
apoorva <apoorvab.22.beis@acharya.ac.in>
2026-02-22 15:00:04 +0530

    Initial commit

---
+++ PersonalBioCard.py
@@ -0,0 +1,28 @@
+#Write a program that creates a personal bio card in formatted card
+name="Apoorva Biradar"
+age= 22
+course="Python programming"
+college="ABC University"
+Email="apoorva@example.com"
+
+print(" ||===============================||")
+print(" ||       STUDENT BIO CARD        ||")
+print(" ||===============================||")
+print(f" || Name    : {name}     ||")
+print(f" || Age     : {age} years            ||")
+print(f" || Course  : {course}  ||")
+print(f" || College : {college}      ||")
+print(f" || Email   : {Email} ||")
+print(" ||===============================||")
+
+
+# Output:
+#  ||===============================||
+#  ||       STUDENT BIO CARD        ||
+#  ||===============================||
+#  || Name    : Apoorva Biradar     ||
+#  || Age     : 22 years            ||
+#  || Course  : Python programming  ||
+#  || College : ABC University      ||
+#  || Email   : apoorva@example.com ||
+#  ||===============================||
